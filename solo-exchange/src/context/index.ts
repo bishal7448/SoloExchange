f6cb148dfea3fc62b7ece0aa28b7b95c32770516
Bishal Saha <saha.bishal7676@gmail.com>
2025-04-18 17:40:02 +0530

    add some comment to index.ts

--- solo-exchange/src/context/index.ts
+++ solo-exchange/src/context/index.ts
@@ -1,1 +1,5 @@
+/**
+ * Exports the default SoloExchangeContext from the './SoloExchangeContext' module.
+ * This context can be used to manage and access the state of the SoloExchange application.
+ */
 export { default as SoloExchangeContext } from './SoloExchangeContext';
