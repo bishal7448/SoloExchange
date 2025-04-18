0796afd05717546e479ee5b131db91da0b77fd64
Bishal Saha <saha.bishal7676@gmail.com>
2025-04-18 17:20:38 +0530

    updated

---
+++ solo-exchange/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // Specify the paths to all of the template files in your project
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      // Extend the default theme here
+    },
+  },
+  plugins: [
+    // Add Tailwind CSS plugins here
+  ],
+}
