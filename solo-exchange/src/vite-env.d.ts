7cd3aa0793506dcc6d63efac7f0cd5ae78b9e5f4
Bishal Saha <saha.bishal7676@gmail.com>
2025-04-19 23:56:26 +0530

    add some required

--- solo-exchange/src/vite-env.d.ts
+++ solo-exchange/src/vite-env.d.ts
@@ -1,14 +1,5 @@
 /// <reference types="vite/client" />
 
-/**
- * Extends the global `Window` interface to include a custom property `ethereum`.
- * This property is commonly used in blockchain-related applications to interact
- * with Ethereum-compatible wallets, such as MetaMask.
- * 
- * @property ethereum - Represents an Ethereum provider object, typically injected
- * by browser extensions or wallet applications. The type is set to `any` to allow
- * flexibility, but it is recommended to use a more specific type if available.
- */
 interface Window {
     ethereum: any
 }
